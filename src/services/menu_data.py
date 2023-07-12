ba9ad3bed417d12a394522d8bb5f89b379e6d8ad
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-12 20:37:39 -0300

    req3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,28 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set(self.insert_menu_datas(source_path))
+
+    def insert_menu_datas(self, source_path):
+        with open(source_path, encoding="utf8", newline="") as file:
+            data = csv.DictReader(file)
+            dishes = set()
+            for content in data:
+                dish_name = content["dish"]
+                dish_price = float(content["price"])
+                dish_ingredient = content["ingredient"]
+                dish_recipe_amount = int(content["recipe_amount"])
+
+                dishes.add(Dish(dish_name, dish_price))
+
+                index_dish = next(iter(dishes))
+                index_dish.add_ingredient_dependency(
+                    Ingredient(dish_ingredient), dish_recipe_amount
+                )
+
+            return dishes
